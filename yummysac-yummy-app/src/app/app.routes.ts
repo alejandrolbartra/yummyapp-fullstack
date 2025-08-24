cd560530970b3d04c4184e751697bf1bb7786950
alejandrolbartra <alejandrolbartra@outlook.com>
2025-08-24 10:06:56 -0300

    tiene vista ahora

--- yummysac-yummy-app/src/app/app.routes.ts
+++ yummysac-yummy-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {ListadoComponent} from './features/listado/listado.component';
+import {LoginComponent} from './features/login/login.component';
+import {CrearComponent} from './features/crear/crear.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'listado', component: ListadoComponent },
+  { path: 'crear', component: CrearComponent },
+];
